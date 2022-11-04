2482d2d59880bd885bd4d4e4211d581ad88742ed
Kot <kotvladimir89@mail.ru>
2022-11-05 00:25:27 +0500

    new direct homework

---
+++ Homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA = new Random().Next(-50, 50); // в А и В любые числа
+int numberB = new Random().Next(-50, 50);
+int max = 0;
+int min = 0;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+if (numberA > numberB)
+{
+    max = numberA;
+    min = numberB;
+}
+else
+{
+    max = numberB;
+    min = numberA;
+}
+Console.Write("max = " + max + ",");
+Console.Write("min = " + min);
---
+++ Homework4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+int numberA = new Random().Next(-50, 50);
+int numberB = new Random().Next(-50, 50);
+int numberC = new Random().Next(-50, 50);
+int max = numberA;
+
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberC);
+
+if (numberB > numberA)
+{
+    max = numberB;
+}
+if (numberC > numberB)
+{
+    max = numberB;
+}
+Console.Write("max = " + max + ",");
