5527925638c0d386f22d4e2c36c91098f038666a
Kot <kotvladimir89@mail.ru>
2022-11-08 18:34:59 +0500

    add HW25

---
+++ HW4/Homework25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите метод, который принимает на вход
+// два числа (A и B) и возводит число A в целую степень B.
+// без использования math.pow
+
+int Stepen(int numbA, int numbB)
+{
+   int result = 1;
+    for (int i = 1; i <= numbB; i++)
+    {
+        result = result * numbA;
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numbA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень B: ");
+int numbB = Convert.ToInt32(Console.ReadLine());
+
+int Expon = Stepen(numbA, numbB);
+Console.Write("ответ  " + Expon);
+
+
---
+++ HW4/Homework27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход 
+//  число и выдаёт сумму цифр в числе.
+
+
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+
+void Summa(string[] args)
+{
+    int sum = 0;
+    while (N > 0)
+    {
+        sum += N % 10;
+        N /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+
+
+int Expon = Stepen(numbA, numbB);
+Console.Write("ответ  " + Summa(string[] args));
