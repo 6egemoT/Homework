a80dd705c5da5ed48c3b79af239e179db7becea4
Kot <kotvladimir89@mail.ru>
2022-11-13 19:45:11 +0500

    add HW36

---
+++ HW5/Homework36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+
+
+Console.Write($"Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int RandomNumbers(int arraySize, int min, int max)
+{
+    int[] randomNumbers = new int[arraySize];
+    int sumSize = 0;
+    Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i < randomNumbers.Length; i++)
+    {
+        randomNumbers[i] = new Random().Next(min, max);
+
+        Console.Write(randomNumbers[i] + " ");
+
+        if (i % 2 != 1)
+        {
+            sumSize += randomNumbers[i];
+        }
+    }
+    return sumSize;
+}
+
+int randomNumbers = RandomNumbers(arraySize, 1, 10);
+
+Console.WriteLine($"Сумма элементов, нечётных позиций: {randomNumbers}");
